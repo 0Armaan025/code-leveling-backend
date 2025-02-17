62d5632c7f03fed097d3630372788a10a85e1da1
0Armaan025 <armaan33000@gmail.com>
2025-02-17 23:48:06 +0530

    something, im sick sorry

--- src/routes/logRoutes.js
+++ src/routes/logRoutes.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 
 const router = express.Router();
 
-router.get("/", async (req, res)=> {
-    res.status(200).json({message: "Logs 200 success!"});
+router.get("/", async (req, res) => {
+    res.status(200).json({ message: "Logs 200 success!" });
 });
 
-router.get("/update", async (req, res)=> {
+router.get("/update", async (req, res) => {
 
-    const {apiKey} = req.body;
-    
+    const { apiKey } = req.body;
 
-});p
+
+
+
+});
